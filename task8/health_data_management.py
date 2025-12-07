f722bbd16c0479429944ecd08f47a9fbc8b55c44
enesemreeravci <enes.nev@gmail.com>
2025-12-07 14:04:42 +0100

    task08 done

--- task8/health_data_management.py
+++ task8/health_data_management.py
@@ -1,4 +1,5 @@
 import csv
+from urllib3.filepost import writer
 
 # function help us to load file while using try except.
 def load_data(file_name):
@@ -36,4 +37,31 @@ def process_row(row):
         return [patient_id, name, age, height, weight, "Error: Invalid number"]
 
 def save_data(processed_rows, output_file):
-    # write here
+    try:
+        with open(output_file, "w",  newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Patient ID", "Name", "Age", "Height", "Weight", "BMI"])
+            for row in processed_rows:
+                writer.writerow(row)
+    except FileNotFoundError:
+        print("Error occurred while writing the file")
+        return None
+
+def main():
+    try:
+        rows = load_data("patient_data.csv")
+        if rows is None:
+            print("Input file not found")
+            return
+        data_rows = rows[1:] # for skipping header row, means write everyhting except header
+        processed_rows = []
+        for row in data_rows:
+            result = process_row(row)
+            processed_rows.append(result)
+        save_data(processed_rows,"processed_patient_data.csv")
+    except Exception as e:
+        print("Unexpected Error:", e)
+
+
+if __name__ == "__main__":
+    main()
