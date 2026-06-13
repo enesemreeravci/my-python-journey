f17d5d80fe0fc3d41e0dfc6eff53ac9b757f702d
enesemreeravci <enes.nev@gmail.com>
2026-06-13 18:51:17 +0200

    Added user registiration

--- MyFlask/app.py
+++ MyFlask/app.py
@@ -1,26 +1,78 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
+DATABASE = "users.db"
+
+def create_table():
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   username TEXT NOT NULL,
+                   email TEXT NOT NULL,
+                   password TEXT NOT NULL
+                   )
+                   """)
+    connection.commit()
+    connection.close()
+
 @app.route("/")
 def home():
-    return "hello"
-        
-@app.route("/index") 
-def hello():
-    name = request.args.get("name")
-    age = request.args.get("age")
-    return render_template("index.html", name=name, age=age)
+    return redirect("/register")
 
-@app.route("/calculate")
-def calculate():
-    num1 = request.args.get("num1", type = int)
-    num2 = request.args.get("num2", type = int)
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        connection = sqlite3.connect(DATABASE)
+        cursor = connection.cursor()
+
+        cursor.execute("""
+                INSERT INTO users (username, email, password)
+                VALUES (?, ?, ?)
+                """, (username, email, password))
+        
+        connection.commit()
+        connection.close()
+
+        return redirect("/users")
     
-    result = None
-    if num1 is not None and num2 is not None:
-        result = num1 + num2
-    return render_template("calculate.html", num1=num1, num2=num2, result = result)    
+    return render_template("register.html")
+
+@app.route("/delete/<int:user_id>", methods=["POST"])
+def delete(user_id):
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+
+    cursor.execute(
+            "DELETE FROM users WHERE id = ?",
+            (user_id,)
+        )
+
+    connection.commit()
+    connection.close()
+
+    return redirect("/users")
     
+@app.route("/users")
+def user():
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT id, username, email FROM users")
+    users = cursor.fetchall()
+
+    connection.close()
+
+    return render_template("users.html", users=users)
+
 if __name__ == "__main__":
+    create_table()
     app.run(debug=True)
